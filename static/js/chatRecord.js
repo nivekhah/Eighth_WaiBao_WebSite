c0afeb938f8e819c09a15c8c7b9eb9e817b44fd0
lxf <1165527916@qq.com>
2017-07-27 00:19:47 +0800

    init

---
+++ static/js/chatRecord.js
@@ -0,0 +1,14 @@
+/**
+ * Created by Luminary on 2017/7/19.
+ */
+if(document.getElementById("checkbox-1").checked)
+{
+    document.getElementById("change").innerHTML="机器人";
+}
+if(document.getElementById("checkbox-2").checked)
+{
+    document.getElementById("change").innerHTML="lhl";
+}
+function all() {
+    document.getElementById("change").innerHTML="所有客服";
+}
---
+++ static/js/satisfaction.js
@@ -0,0 +1,10 @@
+/**
+ * Created by Luminary on 2017/7/19.
+ */
+if(document.getElementById("checkbox-2").checked)
+{
+    document.getElementById("change").innerHTML="lhl";
+}
+function all() {
+    document.getElementById("change").innerHTML="所有客服";
+}
